6c12cbdb7b6757afc96836b45b599832b9f02f77
Caio <caiosant10@gmail.com>
2019-05-20 11:55:00 -0300

    pegando valores dos campos

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-var titulo = document.querySelector("#exampleInputTitulo");
-var numero = document.querySelector("#exampleInputNumero");
-var estado = document.querySelectorAll("#form-hq-create [name=estado]:checked");
-var lancamento = document.querySelector("#exampleInputLancamento");
-var genero = document.querySelector("#eexampleInputGenero");
-var saga = document.querySelector("#exampleInputSaga");
-var photo = document.querySelector("#exampleInputFile");
-var foiLido = document.querySelector("#exampleInputLido");
-
-
 var fields = document.querySelectorAll("#form-hq-create [name]");
-fields.forEach(function(field, index){
+var user = {};
 
-    if(field.name == "estado" ){
+
+document.getElementById("form-hq-create").addEventListener("submit", function(event){
+    event.preventDefault();
         
-        if(field.checked){
+    fields.forEach(function(field, index){
 
-            console.log("SIM", field);
-        }
-
-    } else {
-        console.log("NÂO");
-
-    }
+        if(field.name == "estado" ){
+            
+            if(field.checked){
+                user[field.name] = field.value;
+            }
     
+        } else {
+            user[field.name] = field.value;
+    
+        }
+    });
+    
+    console.log(user);
 
+    
 });
