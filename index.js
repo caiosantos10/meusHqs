057e1333b68eeee17499790719a191e788cb0fca
Caio <caiosant10@gmail.com>
2019-05-20 12:19:45 -0300

    adicionando linha com dados preenchidos

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var fields = document.querySelectorAll("#form-hq-create [name]");
-var user = {};
+var hq = {};
 
+function addLine(dataHq){
+    var tr = document.createElement("tr");
+    tr.innerHTML = `<tr>
+                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                    <td>${dataHq.titulo}</td>
+                    <td>${dataHq.universo}</td>
+                    <td>${dataHq.lido}</td>
+                    <td>${dataHq.lancamento}</td>
+                    <td>
+                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                  </tr>
+                     `
+
+    document.getElementById("table-hqs").appendChild(tr);
+}
 
 document.getElementById("form-hq-create").addEventListener("submit", function(event){
     event.preventDefault();
@@ -10,16 +27,17 @@ document.getElementById("form-hq-create").addEventListener("submit", function(ev
         if(field.name == "estado" ){
             
             if(field.checked){
-                user[field.name] = field.value;
+                hq[field.name] = field.value;
             }
     
         } else {
-            user[field.name] = field.value;
+            hq[field.name] = field.value;
     
         }
     });
-    
-    console.log(user);
+    //console.log(hq);
+
+    addLine(hq);
 
     
 });
